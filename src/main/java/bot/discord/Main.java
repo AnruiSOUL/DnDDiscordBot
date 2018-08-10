8740ad8b6346474baabab2f09754590acc3f79cd
anruisoul <anruisoul@monstersbistro.com>
2018-08-10 13:35:19 -0400

    adding in heroku

--- src/main/java/bot/discord/Main.java
+++ src/main/java/bot/discord/Main.java
@@ -1,4 +1,33 @@
 package bot.discord;
 
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.OnlineStatus;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
+import java.io.File;
+
 public class Main {
+    public static JDA jda;
+    public static void main(String[] args) {
+        ClassLoader classLoader = Main.class.getClassLoader();
+        File token = new File(classLoader.getResource("token.txt").getFile());
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        builder.setToken(token.toString());
+        builder.setAutoReconnect(true);
+        builder.setStatus(OnlineStatus.DO_NOT_DISTURB);
+
+        try {
+            jda = builder.buildBlocking();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }catch (RateLimitedException e){
+            e.printStackTrace();
+
+        }
+    }
 }
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,8 @@
+import bot.discord.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+
+}
